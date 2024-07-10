0775cd5a25e37656c56e3cab89f7b3792efc3f42
Альберт Миннуллин <minnullin.albert@mail.ru>
2024-07-11 01:29:03 +0500

    Выполнить ТЗ спринта №6. Добавить новую функциональность ТЗ: история задач неограничена, сохранять только один последний экземпляр задачи, обновить старые и добавить новые unit - тесты. Выполнить проверку.

--- src/ru/practicum/tasktracker/manager/InMemoryHistoryManager.java
+++ src/ru/practicum/tasktracker/manager/InMemoryHistoryManager.java
@@ -88,33 +88,3 @@ public class InMemoryHistoryManager implements HistoryManager {
         myLinkedList.removeNode(myLinkedList.getNode(id));
     }
 }
-
-class Node {
-    private Task task;
-    private Node prev;
-    private Node next;
-
-    public Node getPrev() {
-        return prev;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public Task getTask() {
-        return task;
-    }
-
-    public void setPrev(Node prev) {
-        this.prev = prev;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
-    public void setTask(Task task) {
-        this.task = task;
-    }
-}
